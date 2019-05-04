86374e9c6d48629369587cd91e589f3aa9d90742
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-05-04 22:59:14 +0100

    Add Country model

---
+++ db/migrate/20190504215839_create_countries.rb
@@ -0,0 +1,10 @@
+class CreateCountries < ActiveRecord::Migration[5.2]
+  def change
+    create_table :countries do |t|
+      t.string :name
+      t.string :info
+
+      t.timestamps
+    end
+  end
+end
