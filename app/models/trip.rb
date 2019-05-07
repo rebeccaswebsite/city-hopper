58e3beb1015ebfc93cd9d7b6dadb91da0f67fb65
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-05-07 14:48:44 +0100

    Add city drop down select for trip form

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,4 +3,8 @@ class Trip < ApplicationRecord
   has_many :city_trips
   has_many :cities, through: :city_trips
   validates :name, :user_id, presence: true
+
+  def trip_desination
+    self.cities.map {|city| city }
+  end
 end
