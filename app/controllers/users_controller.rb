a5cf123df79c0d7d0da4ba47e9d456cb045c8275
KevinsVision <us.kevin@icloud.com>
2019-05-07 10:37:20 +0100

    user controller routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,52 @@
 class UsersController < ApplicationController
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def new
+        @user = User.new(user_params)
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @user = User.find(parama[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        
+        @user.update(user_params)
+        if @user.save
+            redirect_to user_path(@user)
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        flash[:notice] = "Your profile has been deleted."
+        redirect_to welcomes_path
+    end
+
+    def method_name
+        
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :bio)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :cities
   resources :countries
   resources :trips
-  resources :users
+  resources :users, only: [:show, :new, :create, :edit, :update, :destroy]
+  resources :welcomes, only: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
